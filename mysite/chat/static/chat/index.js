8db24900ab5640f8a640807e44fd10fcfdf9d28b
Vlad Boldyrev <boldyrev2040@gmail.com>
2022-11-06 20:30:07 +0300

    Доделан интерфейс и добавлен экран поиска

--- mysite/chat/static/chat/index.js
+++ mysite/chat/static/chat/index.js
@@ -1,13 +1,15 @@
+let loadingScreen = document.getElementById("loading-screen");
 let searchButton = document.getElementById("search");
 let stopButton = document.getElementById("stop");
 let form = document.getElementById("form");
+let chatWindow = document.getElementById("chat");
+let chatMessages = document.getElementById("messages");
 const url = `ws://${window.location.host}/ws/chat/`;
 
-startChat();
-// if (localStorage.getItem("chatting") && localStorage.getItem("group_name")) {
-//     startChat();
-// }
-// searchButton.addEventListener("click", startChat);
+if (localStorage.getItem("chatting") && localStorage.getItem("group_name")) {
+    startChat();
+}
+searchButton.addEventListener("click", startChat);
 
 function handleMessage(e) {
     let data = JSON.parse(e.data);
@@ -29,7 +31,6 @@ function handleMessage(e) {
                         </div>
                         <span class="timestamp"><span class="username">${user}</span>&bull;<span class="posttime">${data.time}</span></span>
                     </div>
-                    <img class="user-img" id="user-0" src="//gravatar.com/avatar/56234674574535734573000000000001?d=retro" />
                 </div>
             </article>
             `
@@ -38,7 +39,6 @@ function handleMessage(e) {
             var message = `
                 <article class="msg-container msg-remote">
                 <div class="msg-box">
-                    <img class="user-img" id="user-0" src="//gravatar.com/avatar/00034587632094500000000000000000?d=retro" />
                     <div class="flr">
                         <div class="messages">
                             <p class="msg">
@@ -58,17 +58,27 @@ function handleMessage(e) {
         localStorage.setItem("group_name", data.group_name);
     } else if (data.type === "start") {
         // If backend is ready
-        // Stop loading screen
+        toggleLoadingScreen();
+        toggleChatElements();
     }
 }
 
+function setVisibility(selector, visibility) {
+    selector.style.display = visibility
+}
+
 function toggleChatElements() {
-    searchButton.style.display = (searchButton.style.display == "none") ? "block" : "none";
     stopButton.style.display = (stopButton.style.display == "none") ? "block" : "none";
-    form.style.display = (form.style.display == "none") ? "block" : "none";
+    chatWindow.style.display = (chatWindow.style.display == "none") ? "block" : "none";
+}
+
+function toggleLoadingScreen() {
+    loadingScreen.style.display = (loadingScreen.style.display == "none") ? "block" : "none";
 }
 
 function startChat() {
+    setVisibility(searchButton, "none");
+    toggleLoadingScreen();
     var chatSocket = new WebSocket(url);
     chatSocket.onmessage = function(e) {handleMessage(e)};
     
@@ -76,25 +86,32 @@ function startChat() {
         e.preventDefault();
         let message = e.target.message.value;
         
-        chatSocket.send(JSON.stringify({
-            "message": message
-        }));
-        form.reset();
+        if (message != "") {
+            chatSocket.send(JSON.stringify({
+                "message": message
+            }));
+            form.reset();
+        }
     }
+
     form.addEventListener("submit", formSubmitListener);
     localStorage.setItem("chatting", true);
-    // toggleChatElements();
 
-    // stopButton.addEventListener("click", (e) => {
-    //     chatSocket.close(3000, "User closed chat");
-    // });
+    stopButton.addEventListener("click", (e) => {
+        chatSocket.close(3000, "User closed chat");
+    });
 
     chatSocket.onclose = function(e) {
-        toggleChatElements();
+        setVisibility(searchButton, "block");
+        setVisibility(stopButton, "none");
+        setVisibility(chatWindow, "none");
+        setVisibility(loadingScreen, "none");
         localStorage.clear();
         // Update form event listener 
         // so new WebSocket object will be updated as well 
         form.removeEventListener("submit", formSubmitListener);
+        // Clear chat from previous session
+        chatMessages.textContent = "";
     }
 }
 
